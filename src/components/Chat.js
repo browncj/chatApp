20bbc567993a24f211638a21404475e47f537fb8
Chris <cbrown119955@gmail.com>
2017-11-23 20:14:03 -0800

    gifted chat component

--- src/components/Chat.js
+++ src/components/Chat.js
@@ -2,15 +2,34 @@ import React, {Component} from 'react';
 import {View, Text} from 'react-native';
 import PropTypes from 'prop-types';
 
+import {GiftedChat} from 'react-native-gifted-chat';
 
+// so the props are kind of like the arguments to the constructor
+// that get passed when you create the thing, they should not change
+// over the lifestyle of the component
+
+
+// if something is going to change over the lifestyle of the component,
+// then said data should be stored in its state
 class Chat extends Component {
+    constructor(props){
+        super(props);
+        this.state = {
+            messages: [],
+        };
+    }
+
     render(){
         return(
-            <View>
-                <Text>
-                    Hello {this.props.name_var}
-                </Text>
-            </View>
+            <GiftedChat
+                messages={this.state.messages}
+                onSend={(message) => {
+                    // send message to your backend
+                }}
+                user={{
+                    _id: 1,
+                }}
+            />
         );
     }
 }
