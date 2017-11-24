6944505c9931daae6234e7347cfeba6398158e48
Chris <cbrown119955@gmail.com>
2017-11-23 19:59:55 -0800

    Pass name to chat view

--- src/components/Chat.js
+++ src/components/Chat.js
@@ -1,18 +1,27 @@
 import React, {Component} from 'react';
 import {View, Text} from 'react-native';
+import PropTypes from 'prop-types';
+
 
 class Chat extends Component {
     render(){
         return(
             <View>
                 <Text>
-                    Hello Chat
+                    Hello {this.props.name_var}
                 </Text>
             </View>
         );
     }
 }
 
+Chat.defaultProps = {
+    name_var: '',
+};
+
+Chat.propTypes = {
+    name_var: PropTypes.string,
+};
 
 // make this importable
 export default Chat;
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,7 +1,15 @@
 import React, {Component} from 'react';
-import {View, Text, StyleSheet, TextInput} from 'react-native';
+import {View, Text, StyleSheet, TextInput, TouchableOpacity} from 'react-native';
+
+import {
+    Actions,
+} from 'react-native-router-flux';
 
 class Home extends Component {
+    state = {
+        name: '',
+    }
+
     render(){
         return(
             <View>
@@ -11,7 +19,24 @@ class Home extends Component {
                 <TextInput
                     style={styles.nameInput}
                     placeholder='John Snow'
+                    onChangeText={(text)=>{
+                        this.setState({
+                            name: text,
+                        });
+                    }}
+                    value={this.state.name}
                 />
+                <TouchableOpacity onPress={()=>{
+                    if(!this.state.name){
+                        alert('Error: Name required');
+                    } else {
+                        Actions.chat({
+                            name_var: this.state.name,
+                        });
+                    }
+                }}>
+                    <Text style={styles.buttonText}>Next</Text>
+                </TouchableOpacity>
             </View>
         );
     }
@@ -24,11 +49,16 @@ var styles = StyleSheet.create({
         fontSize: 20,
     },
     nameInput: {
+        padding: 5,
         height: 40,
         borderWidth: 2,
         borderColor: 'black',
         margin: 20,
     },
+    buttonText: {
+        marginLeft: 20,
+        fontSize: 20,
+    }
 });
 
 // make class importable
